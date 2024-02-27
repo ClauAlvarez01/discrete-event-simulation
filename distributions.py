7b54278453cfa7bfe9e838b13574d64ca3c798bb
ClauAlvarez01 <clau200103am@gmail.com>
2024-02-26 20:17:33 -0500

    feat: Add simulation system

--- distributions.py
+++ distributions.py
@@ -1,7 +1,18 @@
 import random
+import numpy as np
 
 def uniform(a, b):
     return random.uniform(a, b)
 
-def exp(lambd):
-    return random.expovariate(lambd)
+def exp(n, lambd):
+    #Generar variables uniformes U
+    u = np.random.rand(n)
+
+    #Función de distribución acumulativa: F(x;λ)=1−e^(−λx), x≥0
+
+    #Método de la inversa -> generar variables aleatorias uniformes U y 
+    #aplicar la inversa de la FDA para obtener las variables aleatorias exponenciales.
+    exponential_variables = -np.log(1 - u) / lambd
+
+    return exponential_variables
+
